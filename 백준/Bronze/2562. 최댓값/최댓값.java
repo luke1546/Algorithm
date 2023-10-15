4e34d97a10e186a028e6482a8885272e3cc3bdf0
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-15 17:59:37 +0900

    [Bronze III] Title: 최댓값, Time: 112 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int max = 0;
+		int maxi=0;
+		int n = 0;
+		for(int i=0; i<9; i++) {
+			n = sc.nextInt();
+			if(max<n) {
+				max = n;
+				maxi=i+1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(maxi);
+	}
+}
