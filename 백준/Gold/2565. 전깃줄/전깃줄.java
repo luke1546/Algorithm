32c2ac39da0e9a7f95b4550b2b74d07ffc101918
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-16 14:50:35 +0900

    [Gold V] Title: 전깃줄, Time: 216 ms, Memory: 18040 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str[];
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());	//전깃줄의 갯수
 		int[][] 전깃줄 = new int[N][2];
+        StringTokenizer st;
 		for(int i=0; i<N; i++) {
-			str = br.readLine().split(" ");
-			전깃줄[i][0] = Integer.parseInt(str[0]);
-			전깃줄[i][1] = Integer.parseInt(str[1]);
+			st = new StringTokenizer(br.readLine()," ");
+			전깃줄[i][0] = Integer.parseInt(st.nextToken());
+			전깃줄[i][1] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(전깃줄,(o1,o2) -> o1[0]-o2[0]);
-//		LinkedList<Integer> ll = new LinkedList<>();
 		
-		LinkedList<Integer> al = new LinkedList<>();
-		for(int i=0; i<전깃줄.length; i++) {
-			int x = Collections.binarySearch(al, 전깃줄[i][1])*-1;
-			if(x==al.size()+1)	al.add(전깃줄[i][1]);
-			else {
-				al.add(x, 전깃줄[i][1]);
-				al.remove(x-1);
-			}
+		int[] 최장증가수열 = new int[N];
+		Arrays.fill(최장증가수열, Integer.MAX_VALUE);
+		int max = 0;
+		for(int i=0; i<N; i++) {
+			int index = (Arrays.binarySearch(최장증가수열, 전깃줄[i][1])*-1)-1;
+			최장증가수열[index] = 전깃줄[i][1];
+			max = Math.max(max, index);
 		}
-		System.out.println(전깃줄.length - al.size());
+		System.out.println(전깃줄.length - (max+1));
 	}
 }
