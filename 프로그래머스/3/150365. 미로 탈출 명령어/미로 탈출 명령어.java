7da42e049708eb8af9314fde6fed705d12400ffa
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-19 20:24:42 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.54 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -3,25 +3,12 @@ class Solution {
         String answer = "";
         StringBuilder sb = new StringBuilder();
         while(getDis(x,y,r,c)<k){
-            if(x<n) {
-                x+=1;
-                sb.append("d");
-            }else if(y>1){
-                y-=1;
-                sb.append("l");
-            }else if(y<m){
-                y+=1;
-                sb.append("r");
-            }else{
-                x-=1;
-                sb.append("u");
-            }
-            // System.out.println(getDis(x,y,r,c));
-            // System.out.println(k);
-            k-=1;
+            if(x<n) {x++;    sb.append("d");
+            }else if(y>1){y--;    sb.append("l");
+            }else if(y<m){ y++;    sb.append("r");
+            }else{x--;    sb.append("u");}
+            k--;
         }
-        // System.out.println(getDis(x,y,r,c));
-        // System.out.println(x+" "+y+" "+r+" "+c+" "+k);
         if(getDis(x,y,r,c)==k) goEnd(x,y,r,c,sb);
         else    return "impossible";
         answer = sb.toString();
@@ -31,14 +18,9 @@ class Solution {
         return (int)(Math.abs(x-r)+Math.abs(y-c));
     }
     private static void goEnd(int x, int y, int r, int c, StringBuilder sb){
-        if(x<r)
-            for(int i=0; i<r-x; i++)    sb.append("d");
-        if(c<y)
-            for(int i=0; i<y-c; i++)    sb.append("l");
-        if(y<c)
-            for(int i=0; i<c-y; i++)    sb.append("r");
-        if(r<x)
-            for(int i=0; i<x-r; i++)    sb.append("u");
-        
+        if(x<r) for(int i=0; i<r-x; i++)    sb.append("d");
+        if(c<y) for(int i=0; i<y-c; i++)    sb.append("l");
+        if(y<c) for(int i=0; i<c-y; i++)    sb.append("r");
+        if(r<x) for(int i=0; i<x-r; i++)    sb.append("u");
     }
 }
