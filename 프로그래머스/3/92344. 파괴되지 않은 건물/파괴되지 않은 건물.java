cc3b3e28caa9742cc812512854741e2d57b4d54e
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-25 14:01:05 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 49.43 ms, Memory: 218 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -12,20 +12,19 @@ class Solution {
             map[skill[size][3]+1][skill[size][2]] += (skill[size][5]*-1);
             map[skill[size][3]+1][skill[size][4]+1] += (skill[size][5]);
         }
-        // System.out.println(Arrays.deepToString(map));
         
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 map[i][j+1] += map[i][j];
             }
         }
-        // System.out.println(Arrays.deepToString(map));
+        
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 map[i+1][j] += map[i][j];
             }
         }
-        // System.out.println(Arrays.deepToString(map));
+        
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 map[i][j] += board[i][j];
@@ -37,7 +36,7 @@ class Solution {
                 if(map[i][j] > 0) answer++;
             }
         }
-        // System.out.println(Arrays.deepToString(board));
+        
         return answer;
     }
 }
