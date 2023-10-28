aa65d994b6573c5eedc570be03fd11159f7ca473
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-28 12:04:30 +0900

    [Bronze III] Title: 최소, 최대, Time: 992 ms, Memory: 128396 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,15 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int arr[] = new int[N];
-		for(int i=0; i<N; i++) arr[i] = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
 		Arrays.sort(arr);
 		System.out.print(arr[0] + " ");
 		System.out.print(arr[arr.length-1]);
-		sc.close();
 	}
 }
