1f7e5884565e1e4d033e93daa3fce6a88c6b8403
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-31 09:52:42 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1552 ms, Memory: 333412 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int V, E, parents[];
+	static ArrayList<ArrayList<int[]>> 그래프 = new ArrayList<>();
+	static PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> o1[2]-o2[2]);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		for(int i=0; i<V; i++) 그래프.add(new ArrayList<>());
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int from = Integer.parseInt(st.nextToken())-1;
+			int to = Integer.parseInt(st.nextToken())-1;
+			int weight = Integer.parseInt(st.nextToken());
+//			그래프.get(from).add(new int[] {to,weight});
+			pq.offer(new int[] {from, to, weight});
+		}
+		make();
+		int ans=0;
+		int cnt=0;
+		int max=0;
+		while(!pq.isEmpty()) {
+			if(cnt==V-1) break;
+			int cur[] = pq.poll();
+			if(union(cur[0],cur[1])) {
+				cnt++;
+				ans+=cur[2];
+				max = Math.max(max, cur[2]);
+			}
+		}
+		System.out.println(ans-max);
+	}
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if(aRoot == bRoot) return false;
+		parents[bRoot] = aRoot;
+		return true;
+	}
+	private static int find(int a) {
+		if(parents[a] == a)	return a;
+		return parents[a] = find(parents[a]);
+	}
+	private static void make() {
+		parents = new int[V];
+		for(int i=0; i<V; i++)	parents[i] = i;
+	}
+}
