b8db191b65fefc1f7108831ae8f327abc18fd91a
luke1546 <77063001+luke1546@users.noreply.github.com>
2023-10-31 09:54:18 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1344 ms, Memory: 332668 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,27 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int V, E, parents[];
-	static ArrayList<ArrayList<int[]>> 그래프 = new ArrayList<>();
 	static PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> o1[2]-o2[2]);
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
-		for(int i=0; i<V; i++) 그래프.add(new ArrayList<>());
 		for(int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			int from = Integer.parseInt(st.nextToken())-1;
 			int to = Integer.parseInt(st.nextToken())-1;
 			int weight = Integer.parseInt(st.nextToken());
-//			그래프.get(from).add(new int[] {to,weight});
 			pq.offer(new int[] {from, to, weight});
 		}
 		make();
