fbfc186f825214c62035bfe85629b3e632fb1251
luke1546 <77063001+luke1546@users.noreply.github.com>
2024-01-05 14:09:55 +0900

    [Gold I] Title: 구간 합 구하기, Time: 524 ms, Memory: 115120 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,56 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static long segment[];
+	static long[] segment;
+	static int size, half, N, M, K;
+	static StringTokenizer st;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		makeSegment(N);
-		int size = segment.length;
-		for(int i=size/2; i<size/2 + N; i++)
-			segment[i] = Long.parseLong(br.readLine());
-		
-		for(int i=size-1; i>1; i--)
-			segment[i/2]+=segment[i];
-		
-		for(long i=0; i<M+K; i++) {
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		make();
+		for(int i=0; i<M+K; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			long a = Long.parseLong(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			long c = Long.parseLong(st.nextToken());
-			if(a==1) {
-				int index = (b-1+size/2);
-				long dif = c - segment[index];
-				while(index > 0) {
-					segment[index]+=dif;
-					index/=2;
-				}
-			}else {
-				int start = (b-1+size/2);
-				int end = (int) (c-1+size/2);
-				long ans = 0;
-				while(end >= start) {
-					if(start%2==1)	ans+=segment[start];
-					if(end%2==0) ans+=segment[end];
-					start = (start+1)/2;
-					end = (end-1)/2;
-				}
-				sb.append(ans).append("\n");
-			}
+			if(Integer.parseInt(st.nextToken()) == 2)	query();
+			else update();
 		}
-		System.out.print(sb.toString());
+		System.out.println(sb.toString());
 	}
-
-
-	private static void makeSegment(int N) {
-		segment = new long[(int)Math.pow(2, Math.ceil(Math.log(N) / Math.log(2)))*2];
+	private static void update() {
+		int b = Integer.parseInt(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
+		b+=(half-1);
+		long dif = segment[b] - c;
+		while(b>0) {
+			segment[b]-=dif;
+			b/=2;
+		}
+	}
+	private static void query() {
+		long answer=0;
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		b+=(half-1);
+		c+=(half-1);
+		while(c>=b) {
+			if(b%2==1)	answer+=segment[b++];
+			if(c%2==0)	answer+=segment[c--];
+			b/=2;
+			c/=2;
+		}
+		sb.append(answer + "\n");
+	}
+	private static void make() throws IOException {
+		size = (int)(Math.pow(2,Math.ceil((Math.log(N) / Math.log(2)))))*2;
+		half = size/2;
+		segment = new long[size];
+		for(int i=half; i<N+half; i++)	segment[i] = Long.parseLong(br.readLine());
+		for(int i = size-1; i>0; i-=2)	segment[i/2] = segment[i]+segment[i-1];
 	}
 }
+
+/*
+bj_2042 구간 합 구하기
+input:
+5 2 2
+1
+2
+3
+4
+5
+1 3 6
+2 2 5
+1 5 2
+2 3 5
+answer:
+17
+12
+ */
