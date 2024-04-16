03ecbc4e70c28ff5c8201f6c6cec0b74e852bcb4
luke1546 <77063001+luke1546@users.noreply.github.com>
2024-04-16 12:24:49 +0900

    [level 3] Title: 표 편집, Time: 328.38 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,33 +1,74 @@
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.*;
+
+class Node{
+    int id;
+    Node next;
+    Node prev;
+    Node(int id){
+        this.id = id;
+    }
+}
+
 class Solution {
+    static Node curNode;
+    static Stack<Node> stack = new Stack<>();
+    static int size;
     public String solution(int n, int k, String[] cmd) {
-        Stack<Integer> remove_order = new Stack<Integer>();
-        int table_size = n;
-        for(int i=0; i<cmd.length; i++) {
-            char c = cmd[i].charAt(0);
-            if(c=='D')
-                k+=Integer.parseInt(cmd[i].substring(2));
-            else if(c=='U')
-                k-=Integer.parseInt(cmd[i].substring(2));
-            else if(c=='C') {
-                remove_order.add(k);
-                table_size--;
-                if(k==table_size)
-                    k--;
-            }
-            else if(c=='Z') {
-                if(remove_order.pop()<=k)
-                    k++;
-                table_size++;
-            }
+        size = n;
+        ArrayList<Node> nodes = new ArrayList<>();
+        for(int i=0; i<n; i++) nodes.add(new Node(i));
+        nodes.get(0).next = nodes.get(1);
+        nodes.get(n-1).prev = nodes.get(n-2);
+        for(int i=1; i<n-1; i++){
+            nodes.get(i).prev = nodes.get(i-1);
+            nodes.get(i).next = nodes.get(i+1);
         }
-        StringBuilder builder = new StringBuilder();
-        for(int i=0; i<table_size; i++)
-            builder.append("O");
-        while(!remove_order.isEmpty())
-            builder.insert(remove_order.pop().intValue(), "X");
-        String answer=builder.toString();
-        return answer;
+        curNode = nodes.get(k);
+        for(int i=0; i<cmd.length; i++){
+            String order[] = cmd[i].split(" ");
+            if(order[0].equals("D")) move(0, Integer.parseInt(order[1]));
+            else if(order[0].equals("U")) move(1, Integer.parseInt(order[1]));
+            else if(order[0].equals("C")) delete();
+            else restore();
+        }
+        StringBuilder answer = new StringBuilder("O".repeat(n));
+        
+        while(!stack.empty()) {
+        Node node = stack.pop();
+        answer.setCharAt(node.id, 'X');
+        }
+        return answer.toString();
+    }
+    
+    void move(int d, int dis){
+        for(int i=0; i<dis; i++) {
+            if(d==0) curNode = curNode.next;
+            else curNode = curNode.prev;
+        }
+    }
+    
+    void delete(){
+        stack.push(curNode);
+        Node preNode = curNode.prev;
+        Node nextNode = curNode.next;
+        if(preNode==null && nextNode != null){
+            nextNode.prev = null;
+            curNode = nextNode;
+        }else if(preNode != null && nextNode == null){
+            preNode.next = null;
+            curNode = preNode;
+        }else{
+            preNode.next = nextNode;
+            nextNode.prev = preNode;
+            curNode = nextNode;
+        }
+    }
+    
+    void restore(){
+        Node reNode = stack.pop();
+        Node preNode = reNode.prev;
+        Node nextNode = reNode.next;
+        if(preNode != null) preNode.next = reNode;
+        if(nextNode != null) nextNode.prev = reNode;
     }
 }
