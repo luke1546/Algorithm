2fc42b1ff2b69c791c0be86fd4f04527c8098760
hoseong <77063001+luke1546@users.noreply.github.com>
2024-05-27 17:07:00 +0900

    [Silver II] Title: 에디터, Time: 548 ms, Memory: 129324 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -9,44 +9,34 @@ public class Main {
 		Node right;
 		Node left;
 		char c;
-		public Node(char c) {
-			this.c = c;
-		}
-		public Node(char c, Node left, Node right) {
+		Node(){}		
+		Node(char c) { this.c = c; }
+		Node(char c, Node left, Node right) {
 			this.c = c;
 			this.left = left;
 			this.right = right;
-		}
-		@Override
-		public String toString() {
-			return this.c+"";
 		}
 	}
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
-		String str = br.readLine();
-		int N = str.length();
+		char[] carr = br.readLine().toCharArray();
+		int N = carr.length;
 		int M = Integer.parseInt(br.readLine());
-		ArrayList<Node> nodes = new ArrayList<>();
-		nodes.add(new Node(' '));
-		for(int i=0; i<N; i++) nodes.add(new Node(str.charAt(i)));
-		nodes.get(0).right = nodes.get(1);
-		nodes.get(N).left = nodes.get(N-1);
-		nodes.get(0).left = null;
-		nodes.get(N).right = null;
-		for(int i=1; i<N; i++) {
-			nodes.get(i).left = nodes.get(i-1);
-			nodes.get(i).right = nodes.get(i+1);
+		Node curNode = new Node(), head = curNode;
+		for(int i=0; i<N; i++) {
+			Node newNode = new Node(carr[i]);
+			curNode.right = newNode;
+			newNode.left = curNode;
+			curNode = newNode;
 		}
-		Node curNode = nodes.get(N);
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			char ch = st.nextToken().charAt(0);
 			if(ch == 'L' && curNode.left != null) curNode = curNode.left;
 			else if(ch == 'D' && curNode.right != null) curNode = curNode.right;
-			else if(ch == 'B' && curNode.c != ' ') {
+			else if(ch == 'B' && curNode != head) {
 				Node rightNode = curNode.right;
 				curNode = curNode.left;
 				curNode.right = rightNode;
@@ -59,13 +49,12 @@ public class Main {
 				if(rightNode != null) rightNode.left = newNode;
 			}
 		}
-		Node initNode = nodes.get(0);
+		Node initNode = head.right;
 		while(initNode != null) {
 			sb.append(initNode.c);
 			initNode = initNode.right;
 		}
-		sb.delete(0,1);
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 }
 
