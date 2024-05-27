53ccc0406c455a1355ba420b89fc3cc92076b082
hoseong <77063001+luke1546@users.noreply.github.com>
2024-05-27 14:45:42 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 84 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -53,7 +51,7 @@ public class Main {
         st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        Queue<Room> rooms = new ArrayDeque<>();
+        ArrayList<Room> rooms = new ArrayList<>();
         
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
