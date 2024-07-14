8e6a2bfed0aeba9ec70e17f708cbd97cc71d92a9
hoseong <77063001+luke1546@users.noreply.github.com>
2024-07-15 03:45:25 +0900

    [Gold V] Title: 문자열 게임 2, Time: 264 ms, Memory: 37140 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,46 +1,42 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        //입력
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
- 
-        int t = Integer.parseInt(bf.readLine());
-        for(int i = 0; i < t; i++) {
-            String str = bf.readLine();
-            int k = Integer.parseInt(bf.readLine()); 
-            //입력 끝
- 
-            if(k == 1) { //k가 1일때
-                System.out.println("1 1");
-                continue;
-            }
- 
-            int[] alpha = new int[26];//알파벳 별 개수를 저장한다. 
-            for(int j = 0; j < str.length(); j++) {
-                alpha[str.charAt(j) - 'a']++;
-            }
- 
-            int min = Integer.MAX_VALUE;
-            int max = -1;
-            for(int j = 0; j < str.length(); j++) {
-                if(alpha[str.charAt(j) - 'a'] < k) continue;
- 
-                int count = 1;
-                for(int l = j + 1; l < str.length(); l++) {
-                    if(str.charAt(j) == str.charAt(l)) count++;
-                    if(count == k) {
-                        min = Math.min(min, l - j + 1);
-                        max = Math.max(max, l - j + 1);
-                        break;
-                    }
-                }
-            }
-            if(min == Integer.MAX_VALUE || max == -1) System.out.println("-1");
-            else System.out.println(min + " " + max);
-        }
-    }        
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static Deque<Integer> dq;
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=0; tc<T; tc++) {
+			char cArr[] = br.readLine().toCharArray();
+			int k = Integer.parseInt(br.readLine());
+			int min = Integer.MAX_VALUE;
+			int max = Integer.MIN_VALUE;
+			int arr[] = new int[26];
+			ArrayList<ArrayList<Integer>> al = new ArrayList<>();
+			for(int i=0; i<26; i++) al.add(new ArrayList<>());
+			for(int i=0; i<cArr.length; i++) {
+				int index = cArr[i]-'a';
+				al.get(index).add(i);
+				arr[index]++;
+			}
+			for(int i=0; i<26; i++) {
+				if(arr[i] < k) continue;
+				for(int j=0; j<al.get(i).size()-k+1; j++) {
+					int s = al.get(i).get(j+k-1) - al.get(i).get(j)+1;
+					max = Math.max(max, s);
+					min = Math.min(min, s);
+				}
+			}
+			if(min == Integer.MAX_VALUE) sb.append(-1);
+			else sb.append(min).append(" ").append(max);
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
