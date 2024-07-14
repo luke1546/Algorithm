1f61110c8761b97348b23e5cdd90db8a98331965
hoseong <77063001+luke1546@users.noreply.github.com>
2024-07-15 03:29:08 +0900

    [Gold V] Title: 문자열 게임 2, Time: 420 ms, Memory: 19268 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -1,54 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
+ 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	static Deque<Integer> dq;
-	public static void main(String[] args) throws IOException {
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<T; tc++) {
-			String str = br.readLine();
-			int k = Integer.parseInt(br.readLine());
-			int min = Integer.MAX_VALUE;
-			int max = Integer.MIN_VALUE;
-			int arr[] = new int[26];
-			boolean check[] = new boolean[26];
-			for(int i=0; i<str.length(); i++) arr[str.charAt(i)-'a']++;
-			
-			for(int i=0; i<str.length(); i++) {
-				int index = str.charAt(i)-'a';
-				if(check[index] || arr[index] < k) continue;
-				check[index] = true;
-				dq = new ArrayDeque<>();
-				dq.offer(i);
-				for(int j=i+1; j<str.length(); j++) {
-					if(dq.size() == k) {
-						min = Math.min(min, dq.peekLast()-dq.peekFirst()+1);
-						max = Math.max(max, dq.peekLast()-dq.peekFirst()+1);
-						dq.poll();
-					}
-					if(str.charAt(j)-'a' == index) {
-						dq.offer(j);
-					}
-				}
-				if(dq.size() == k) {
-					min = Math.min(min, dq.peekLast()-dq.peekFirst()+1);
-					max = Math.max(max, dq.peekLast()-dq.peekFirst()+1);
-					dq.poll();
-				}
-			}
-			
-			if(min == Integer.MAX_VALUE || max == Integer.MIN_VALUE) sb.append(-1);
-			else sb.append(min).append(" ").append(max);
-			sb.append("\n");
-		}
-		System.out.print(sb);
-	}
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        //입력
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+ 
+        int t = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < t; i++) {
+            String str = bf.readLine();
+            int k = Integer.parseInt(bf.readLine()); 
+            //입력 끝
+ 
+            if(k == 1) { //k가 1일때
+                System.out.println("1 1");
+                continue;
+            }
+ 
+            int[] alpha = new int[26];//알파벳 별 개수를 저장한다. 
+            for(int j = 0; j < str.length(); j++) {
+                alpha[str.charAt(j) - 'a']++;
+            }
+ 
+            int min = Integer.MAX_VALUE;
+            int max = -1;
+            for(int j = 0; j < str.length(); j++) {
+                if(alpha[str.charAt(j) - 'a'] < k) continue;
+ 
+                int count = 1;
+                for(int l = j + 1; l < str.length(); l++) {
+                    if(str.charAt(j) == str.charAt(l)) count++;
+                    if(count == k) {
+                        min = Math.min(min, l - j + 1);
+                        max = Math.max(max, l - j + 1);
+                        break;
+                    }
+                }
+            }
+            if(min == Integer.MAX_VALUE || max == -1) System.out.println("-1");
+            else System.out.println(min + " " + max);
+        }
+    }        
 }
