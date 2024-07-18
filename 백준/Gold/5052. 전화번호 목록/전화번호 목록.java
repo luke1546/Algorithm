71ec86d6c9582ddc78299608f5bef39850217720
hoseong <77063001+luke1546@users.noreply.github.com>
2024-07-18 19:04:42 +0900

    [Gold IV] Title: 전화번호 목록, Time: 608 ms, Memory: 190076 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,78 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static class Node{
-		public HashMap<Character, Node> childrenMap;
-		public char c;
-		public boolean end;
-		Node(char c){
-			this.c = c;
-			end = false;
-			childrenMap = new HashMap<>();
-		}
-		Node(){
-			end = false;
-			childrenMap = new HashMap<>();
-		}
-//		public boolean find(char c) {
-//			Node node = new Node(c);
-//		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			Node node = (Node)obj;
-			return this.c == node.c;
-		}
-	}
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			boolean flag = false;
-			String strs[] = new String[N];
-			Node root = new Node();
-			for(int i=0; i<N; i++) strs[i] = br.readLine();
-			Arrays.sort(strs, (o1,o2) -> o1.length() - o2.length());
-			for(int i=0; i<N; i++) {
-				Node cur = root;
-				String str = strs[i];
-				for(int j=0; j<str.length(); j++) {
-					char c = str.charAt(j);
-					if(cur.end) flag = true;
-					if(!cur.childrenMap.containsKey(c)) cur.childrenMap.put(c, new Node(c));
-					cur = cur.childrenMap.get(c);
-				}
-				cur.end = true;
-			}
-			if(flag) sb.append("NO\n");
-			else sb.append("YES\n");
-		}
-		System.out.println(sb.toString());
-	}
+    public static class Node {
+        public HashMap<Character, Node> childrenMap;
+        public boolean end;
+
+        Node() {
+            end = false;
+            childrenMap = new HashMap<>();
+        }
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            boolean flag = false;
+            Node root = new Node();
+            for (int i = 0; i < N; i++) {
+                String str = br.readLine();
+                if (flag) continue; // 이미 flag가 true인 경우 계속해서 검사할 필요 없음
+                Node cur = root;
+                for (int j = 0; j < str.length(); j++) {
+                    char c = str.charAt(j);
+                    if (cur.childrenMap.containsKey(c) && cur.childrenMap.get(c).end) {
+                        flag = true;
+                        break;
+                    }
+                    cur = cur.childrenMap.computeIfAbsent(c, k -> new Node());
+                }
+                cur.end = true;
+                if (cur.childrenMap.size() > 0) {
+                    flag = true;
+                }
+            }
+            if (flag) sb.append("NO\n");
+            else sb.append("YES\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
-
-/*
-2
-3
-911
-97625999
-91125426
-5
-113
-12340
-123440
-12345
-98346
-
-NO
-YES
-*/
