1dbea8e05ce6c5c1b5d39d00a56cc04fbf806038
hoseong <77063001+luke1546@users.noreply.github.com>
2024-07-18 19:00:51 +0900

    [Gold IV] Title: 전화번호 목록, Time: 840 ms, Memory: 206304 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -2,10 +2,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static class Node{
+		public HashMap<Character, Node> childrenMap;
+		public char c;
+		public boolean end;
+		Node(char c){
+			this.c = c;
+			end = false;
+			childrenMap = new HashMap<>();
+		}
+		Node(){
+			end = false;
+			childrenMap = new HashMap<>();
+		}
+//		public boolean find(char c) {
+//			Node node = new Node(c);
+//		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			Node node = (Node)obj;
+			return this.c == node.c;
+		}
+	}
+	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
@@ -13,19 +37,21 @@ public class Main {
 		int T = Integer.parseInt(br.readLine());
 		for(int tc=0; tc<T; tc++) {
 			int N = Integer.parseInt(br.readLine());
-			HashSet<String> hs = new HashSet<>();
 			boolean flag = false;
 			String strs[] = new String[N];
+			Node root = new Node();
 			for(int i=0; i<N; i++) strs[i] = br.readLine();
 			Arrays.sort(strs, (o1,o2) -> o1.length() - o2.length());
 			for(int i=0; i<N; i++) {
-				StringBuilder sb2 = new StringBuilder();
+				Node cur = root;
 				String str = strs[i];
 				for(int j=0; j<str.length(); j++) {
-					sb2.append(str.charAt(j));
-					if(hs.contains(sb2.toString())) flag = true;
+					char c = str.charAt(j);
+					if(cur.end) flag = true;
+					if(!cur.childrenMap.containsKey(c)) cur.childrenMap.put(c, new Node(c));
+					cur = cur.childrenMap.get(c);
 				}
-				hs.add(str);
+				cur.end = true;
 			}
 			if(flag) sb.append("NO\n");
 			else sb.append("YES\n");
