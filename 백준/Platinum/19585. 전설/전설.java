3e84d91702dbe333737f0836a362e58de7e1f879
hoseong <77063001+luke1546@users.noreply.github.com>
2024-07-20 00:46:37 +0900

    [Platinum III] Title: 전설, Time: 6028 ms, Memory: 874084 KB -BaekjoonHub

--- 백준/Platinum/19585. 전설/전설.java
+++ 백준/Platinum/19585. 전설/전설.java
@@ -1,99 +1,94 @@
-import java.io.*;
 import java.util.*;
-
-import static java.lang.Math.*;
+import java.io.*;
 
 public class Main{
-
-    private int colorCount, nickNameCount;
-
-    private final Trie colorNameEnterCursor = new Trie('S', false);
-    private final Set<String> nickNameSet = new HashSet<>();
-
-    public static void main(String[] args){
-        (new Main()).solve();
-    }
-
-    private void solve(){
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            String[] read = br.readLine().split(" ");
-            this.colorCount = Integer.parseInt(read[0]);
-            this.nickNameCount = Integer.parseInt(read[1]);
-            this.inputColors(br);
-            this.inputNickNames(br);
-            System.out.print(this.findWinableTeams(br));
-        }catch(IOException IOE){}
-    }
-
-    private void inputColors(BufferedReader br) throws IOException{
-        for(int i = 0; i < colorCount; i++) {
-            String read = br.readLine();
-            this.addStringOnTrie(read, this.colorNameEnterCursor);
-        }
-    }
-
-    private void inputNickNames(BufferedReader br) throws IOException{
-        for(int i = 0; i < nickNameCount; i++){
-            String read = br.readLine();
-            this.nickNameSet.add(read);
-        }
-    }
-
-    private void addStringOnTrie(String string, Trie cursor){
-        for(int i = 0; i < string.length(); i++){
-            Trie trie = new Trie(string.charAt(i), (i == string.length()-1));
-            cursor.addNextTrie(trie);
-            cursor = cursor.nextTrie(string.charAt(i));
-        }
-    }
-
-    private String findWinableTeams(BufferedReader br) throws IOException{
-        int teamCount = Integer.parseInt(br.readLine());
-        StringBuilder resultBuilder = new StringBuilder();
-        for(int i = 0; i < teamCount; i++){
-            String read = br.readLine();
-            List<Integer> matchedPoints = new ArrayList<>();
-            this.markColorPoint(read, this.colorNameEnterCursor, matchedPoints);
-            resultBuilder.append(this.matchNickNamePoint(read, matchedPoints)).append("\n");
-        }
-        return resultBuilder.toString();
-    }
-
-    private void markColorPoint(String string, Trie cursor, List<Integer> matchedPoints){
-        for(int i = 0; i < string.length(); i++){
-            cursor = cursor.nextTrie(string.charAt(i));
-            if(cursor == null) return;
-            if(cursor.isEnd && string.length() - (i+1) <= 1000) matchedPoints.add(i);
-        }
-    }
-
-    private String matchNickNamePoint(String string, List<Integer> matchedPoints){
-        for (Integer matchedPoint : matchedPoints)
-            if (this.nickNameSet.contains(string.substring(matchedPoint+1))) return "Yes";
-        return "No";
-    }
-
-    private static class Trie{
-        char name;
-        Trie[] nextNames = new Trie[(int)'z'-(int)'a' + 1];
+    public static class Node{
+    	char color;
+        Node children[];
         boolean isEnd;
-
-        public Trie(char name, boolean isEnd){
-            this.name = name;
-            this.isEnd = isEnd;
+        public Node(char color){
+        	this.color = color;
+            children = new Node[26];
+            isEnd = false;
         }
-
-        public void addNextTrie(Trie trie){
-            int nextNameIdx = (int)trie.name - (int)'a';
-            if(nextNames[nextNameIdx] == null) nextNames[nextNameIdx] = trie;
-            nextNames[nextNameIdx].isEnd = nextNames[nextNameIdx].isEnd == true ? true : trie.isEnd;
+        public Node(){
+        	children = new Node[26];
+        	isEnd = false;
         }
-
-        public Trie nextTrie(char nextName){
-            int nextNameIdx = (int)nextName - (int)'a';
-            return nextNames[nextNameIdx];
+        public Node child(char ch) {
+        	return this.children[ch-'a'];
         }
-
     }
+    static int C, N, Q;
+    static Node colorRoot = new Node();
+    static HashSet<String> names = new HashSet<>();
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        C = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        for(int i=0; i<C; i++){
+	        String str = br.readLine();
+	        addWord(colorRoot, str);
+        }
+        for(int i=0; i<N; i++){
+	        String str = br.readLine();
+	        names.add(str);
+        }
+        Q = Integer.parseInt(br.readLine());
+        for(int i=0; i<Q; i++){
+	        String str = br.readLine();
+		    if(colorCheck(str)) sb.append("Yes\n");
+		    else sb.append("No\n");
+        }
+        System.out.println(sb.toString());
+    }
+    
+    private static void addWord(Node root, String str) {
+    	Node curNode = root;
+    	for(int j=0; j<str.length(); j++){
+    		int index = str.charAt(j)-'a';
+	        if(curNode.children[index] == null) curNode.children[index] = new Node(str.charAt(j));
+	        curNode = curNode.children[index];
+        }
+        curNode.isEnd = true;
+	}
 
+	private static boolean colorCheck(String str){
+	    Node curNode = colorRoot;
+    	boolean flag = false;
+	    for(int i=0; i<str.length(); i++){
+	    	if(flag) break;
+    		int index = str.charAt(i)-'a';
+    		if(curNode.isEnd && str.length() - i <= 1000 && names.contains(str.substring(i))) return true;
+		    if(curNode.children[index] != null) {
+		    	curNode = curNode.children[index];
+		    }else break;
+	    }
+	    return flag;
+    }
 }
+
+/*
+4 3
+red
+blue
+purple
+orange
+shift
+joker
+noon
+5
+redshift
+bluejoker
+purplenoon
+orangeshift
+whiteblue
+
+Yes
+Yes
+Yes
+Yes
+No
+*/
