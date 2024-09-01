1f26116e295b632a35461e7a824c4ea5c0d1fab0
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-01 22:48:33 +0900

    [level 2] Title: 올바른 괄호, Time: 6.32 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,5 +1,11 @@
 class Solution {
     boolean solution(String s) {
-        return s.chars().reduce(0, (cnt, c) -> c == '(' ? cnt + 1 : cnt > 0 ? cnt - 1 : -s.length()) == 0;
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '(') cnt++;
+            else if(cnt > 0) cnt--;
+            else return false;
+        }
+        return (cnt == 0 ? true : false);
     }
 }
