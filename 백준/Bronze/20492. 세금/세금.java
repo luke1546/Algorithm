2fdf38f553a9607e1fa663ad60a6b8401a347ee9
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-04 19:08:44 +0900

    [Bronze V] Title: 세금, Time: 88 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N*78/100 + " " + (N-(N*44/1000)));
+    }
+}
+
+/*
+1000
+
+780 956
+*/
