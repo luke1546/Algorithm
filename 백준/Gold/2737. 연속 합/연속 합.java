5ede8cf14c239bcca1c75f2304d98445b6562597
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-08 22:19:08 +0900

    [Gold III] Title: 연속 합, Time: 68 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Gold/2737. 연속 합/연속 합.java
+++ 백준/Gold/2737. 연속 합/연속 합.java
@@ -1,29 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(br.readLine());
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(br.readLine());
-			check(num);
-		}
+		for(int i=0; i<N; i++) check(Integer.parseInt(br.readLine()));
 		System.out.print(sb);
 	}
+	
 	private static void check(int num) {
-		int cnt = 0, sum = 0;
-		int i = 0;
-		while(sum + i < num) {
-			i++;
-			sum+=i;
-		}
+		int i = 0, cnt = 0, sum = 0;
+		while(sum + i++ < num) sum+=i;
 		while(sum != 0) {
-			if((num - sum) % i == 0) cnt++;
+			if((num - sum) % --i == 0) cnt++;
 			sum-=i;
-			i--;
 		}
 		sb.append(cnt-1).append("\n");
 	}
