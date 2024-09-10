e1538b8b5e7794d26bc2f7c6ac3217d1185073d6
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-10 17:41:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1068 ms, Memory: 214088 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		HashMap<Integer, Integer> hm = new HashMap<>();
+		int N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			hm.merge(Integer.parseInt(st.nextToken()), 1, Integer::sum);
+		}
+		int K = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<K; i++) {
+			int x = Integer.parseInt(st.nextToken());
+			sb.append(hm.get(x) == null ? 0 : hm.get(x)).append(" ");
+		}
+		System.out.print(sb);
+	}
+}
+
+/*
+10
+6 3 2 10 10 10 -10 -10 7 3
+8
+10 9 -5 2 3 4 5 -10
+
+3 0 0 1 2 0 0 2
+*/
