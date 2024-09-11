21f114ecd51b5412093506268bbb80cac2d2a03b
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-11 17:55:17 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 728 ms, Memory: 138820 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -6,21 +6,19 @@ public class Main {
 	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
 		int ans = 0;
-		HashMap<String, Integer> map = new HashMap<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
 		st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		for(int tc=0; tc<N; tc++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			StringBuilder sb = new StringBuilder();
 			int arr[] = new int[M];
+			int rank[] = new int[M];
 			for(int i=0; i<M; i++) arr[i] = Integer.parseInt(st.nextToken());
-			for(int i=0; i<M; i++) sb.append(Arrays.binarySearch(arr, arr[i])).append(" ");
-			String str = sb.toString();
-			if(map.containsKey(str)) map.put(str, map.get(str)+1);
-			else map.put(str, 1);
+			for(int i=0; i<M; i++) rank[i] = Arrays.binarySearch(arr, arr[i]);
+			map.merge(Arrays.hashCode(rank), 1, Integer::sum);
 		}
-		for(String s : map.keySet()) ans+=conbination(map.get(s));
+		for(int key : map.keySet()) ans+=conbination(map.get(key));
 		System.out.print(ans);
 	}
 	private static int conbination(Integer n) {
