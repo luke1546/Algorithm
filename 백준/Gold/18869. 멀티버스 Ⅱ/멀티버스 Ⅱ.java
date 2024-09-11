faca16a89c659bc085dd131f43ec90559c131812
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-11 17:40:40 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 988 ms, Memory: 228312 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -7,19 +7,24 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int ans = 0;
 		HashMap<String, Integer> map = new HashMap<>();
-		map.merge("1 2 ", 1, Integer::sum);
 		st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+//		Integer arr[][] = new Integer[N][M];
 		for(int tc=0; tc<N; tc++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			Integer arr[] = new Integer[M];
-			for(int i=0; i<M; i++) arr[i] = Integer.parseInt(st.nextToken());
-			ArrayList<Integer> list = new ArrayList<>(Arrays.asList(arr));
-			Collections.sort(list);
+			ArrayList<Integer> list = new ArrayList<>();
+			for(int i=0; i<M; i++) {
+//				int x = Integer.parseInt(st.nextToken());
+				list.add(Integer.parseInt(st.nextToken()));
+//				arr[tc][i] = x;
+			}
+//			Collections.sort(list);
 			StringBuilder sb = new StringBuilder();
-			for(int i=0; i<M; i++) sb.append(Collections.binarySearch(list, arr[i])).append(" ");
-			map.merge(sb.toString(), 1, Integer::sum);
+			for(int i=0; i<M; i++) sb.append(Collections.binarySearch(list, list.get(i))).append(" ");
+			if(map.containsKey(sb.toString())) map.put(sb.toString(), map.get(sb.toString())+1);
+			else map.put(sb.toString(), 1);
+//			map.merge(sb.toString(), 1, Integer::sum);
 		}
 		for(String s : map.keySet()) ans+=conbination(map.get(s));
 		System.out.print(ans);
