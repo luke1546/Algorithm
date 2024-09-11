bb7cbf661b4d05e7ba79f349cbc0a31b71ce4077
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-11 18:08:41 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 632 ms, Memory: 135100 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -1,5 +1,9 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
