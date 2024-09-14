14d853d2b9cf9b165ff1724c42b67e0f08630700
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-14 17:18:34 +0900

    [Gold V] Title: 용액 합성하기, Time: 244 ms, Memory: 26780 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -8,37 +10,25 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int arr[];
-	static int min = Integer.MAX_VALUE, ans = 0, N = 0;
+	static int min = Integer.MAX_VALUE, ans, N;
 	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
 		for(int i=0; i<N; i++) {
 			int index = Arrays.binarySearch(arr, -arr[i]);
 			if(index < 0) index = (index*-1)-1;
-			if(index == i) {
-				min(i, index-1);
-				min(i, index+1);
-			}else if(index+1 == i) {
-				min(i, index-1);
-				min(i, index);
-			}else if(index-1 == i) {
-				min(i, index);
-				min(i, index+1);
-			}
-			else {
-				min(i,index);
-				min(i,index+1);
-				min(i, index-1);
-			}
+			min(i,index);
+			min(i,index+1);
+			min(i, index-1);
 		}
-		System.out.println(ans);
+		bw.write(String.valueOf(ans));
+		bw.flush();
 	}
 	private static void min(int i, int j) {
-		if(j >= 0 && j < N) {
+		if(j >= 0 && j < N && i != j) {
 			int v = Math.abs(arr[i] + arr[j]);
 			if(min > v) {
 				min = v;
