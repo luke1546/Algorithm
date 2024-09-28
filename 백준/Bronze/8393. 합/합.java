b9e13a9774d07b5f49bdaed6dffee6fce68ea31d
hoseong <77063001+luke1546@users.noreply.github.com>
2024-09-28 16:23:31 +0900

    [Bronze V] Title: 합, Time: 60 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,14 +1,10 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws Exception {
 		int N = Integer.parseInt(br.readLine());
-		bw.write(String.valueOf(N!=1 ? (1+N)*(N/2)+(N%2*((1+N)/2)) : 1));
-		bw.flush();
+		System.out.print(N!=1 ? (1+N)*(N/2)+(N%2*((1+N)/2)) : 1);
 	}
 }
