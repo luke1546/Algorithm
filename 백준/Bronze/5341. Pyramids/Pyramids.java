f3caae124948d6075baa547c1fad707c070cdc76
hoseong <77063001+luke1546@users.noreply.github.com>
2024-10-17 23:44:20 +0900

    [Bronze V] Title: Pyramids, Time: 88 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int n = sc.nextInt();
+            if(n == 0) break;
+            int ans = 0;
+            for(int i=1; i<=n; i++) ans+=i;
+            System.out.println(ans);
+        }
+    }
+}
