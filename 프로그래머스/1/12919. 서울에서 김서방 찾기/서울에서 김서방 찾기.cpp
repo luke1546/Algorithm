c7e1c2f36219cd6dc905aee5747f9a6d7df917d8
hoseong <77063001+luke1546@users.noreply.github.com>
2025-02-23 16:07:54 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -1,16 +1,13 @@
 #include <string>
 #include <vector>
-
+#include <sstream>
 using namespace std;
 
 string solution(vector<string> seoul) {
-    int index = 0;
-    for(int i=0; i<seoul.size(); i++){
-        if(seoul[i] == "Kim"){
-            index = i;
-            break;
-        }
+    ostringstream oss;
+    for(int i=0; i<seoul.size(); i++) {
+        if(seoul[i] == "Kim") 
+            oss << "김서방은 " << i << "에 있다";
     }
-    string answer = "김서방은 " + to_string(index) + "에 있다";
-    return answer;
+    return oss.str();
 }
