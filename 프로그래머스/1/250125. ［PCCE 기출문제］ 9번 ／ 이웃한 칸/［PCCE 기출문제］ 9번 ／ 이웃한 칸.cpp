0f4ee123adbb7d503ac2e514bbbcb4f1c72bffff
hoseong <77063001+luke1546@users.noreply.github.com>
2025-03-03 23:39:53 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<vector<string>> board, int h, int w) {
+    int answer = 0;
+    int dirs[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+    int R = board.size();
+    int C = board.size();
+    string str = board[h][w];
+    for(auto dir : dirs){
+        int nr = h + dir[0];
+        int nc = w + dir[1];
+        if(nr >= 0 && nr < R && nc >= 0 && nc < C){
+            if(board[nr][nc] == str) answer++;
+        }
+    }
+    return answer;
+}
