d9472ed02e1ff8f11e2662d5fae2ed9d60044c7a
hoseong <77063001+luke1546@users.noreply.github.com>
2025-03-09 00:36:15 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -3,14 +3,6 @@
 
 using namespace std;
 
-void replaceAll(string& s, string from, string to){
-    int pos = 0;
-    while((pos = s.find(from, pos)) != string::npos){
-        s.replace(pos, from.size(), to);
-        pos+=to.size();
-    }
-}
-
 string solution(string s) {
     int index = 0;
     for(auto& c : s){
@@ -18,14 +10,8 @@ string solution(string s) {
             index=0;
             continue;
         }
-        if(index&1) c = tolower(c);
-        else c = toupper(c);
+        index&1 ? c = tolower(c) : c = toupper(c);
         index++;
     }
-    // int pos = 0;
-    // while(s.find("  ") != string::npos)
-    //     replaceAll(s, "  ", " ");
-    // if(s[0] == ' ') s.erase(s.begin());
-    // if(s[s.size()-1] == ' ') s.erase(s.end()-1);
     return s;
 }
