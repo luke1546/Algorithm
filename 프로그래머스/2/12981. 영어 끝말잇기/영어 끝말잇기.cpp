ae00dbc8e982ac1a431f5a5b0bbb9db1fca5eacf
hoseong <77063001+luke1546@users.noreply.github.com>
2025-03-15 16:03:48 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -10,7 +10,6 @@ vector<int> solution(int n, vector<string> words) {
     unordered_set<string> uset;
     int turn = 1;
     int cnt = 0;
-    bool fail = false;
     string before = "";
     for(auto word : words){
         cnt++;
@@ -18,23 +17,9 @@ vector<int> solution(int n, vector<string> words) {
             cnt-=n;
             turn++;
         }
-        if(uset.count(word)) {
-            fail = true;
-            break;
-        }
-        if(!before.empty() && before[before.size()-1] != word[0]) {
-            fail = true;
-            break;
-        }
+        if(uset.count(word) || !before.empty() && before[before.size()-1] != word[0]) return {cnt, turn};
         uset.insert(word);
         before = word;
     }
-    if(fail){
-    answer.push_back(cnt);
-    answer.push_back(turn);
-    }else{
-        answer.push_back(0);
-        answer.push_back(0);
-    }
-    return answer;
+    return {0,0};
 }
