d189e34635e267df4f30a1a3f982e143a379abad
hoseong <77063001+luke1546@users.noreply.github.com>
2025-04-02 22:58:31 +0900

    [Bronze V] Title: 홍익대학교, Time: 96 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Bronze/16394. 홍익대학교/홍익대학교.java
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -1,14 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(br.readLine());
-		System.out.println(N-1946);
-	}
+public class Main{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n-1946);
+    }
 }
