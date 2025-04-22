1428e1d852221220187a38a7952499d4ceb257d4
hoseong <77063001+luke1546@users.noreply.github.com>
2025-04-22 17:56:15 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 244.05 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,77 +1,95 @@
 import java.util.*;
+
 class Solution {
-    static Queue<int[]>[] memo; // 경로 기록용 
-    static int size;
-    static int answer;
+
+    static class Position {
+        int r, c;
+        public Position(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return r == position.r && c == position.c;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(r, c);
+        }
+    }
+
     public int solution(int[][] points, int[][] routes) {
-        size = routes.length;
-        memo = new LinkedList[size];
-        for(int i = 0; i < size; i++){
-            memo[i] = new LinkedList<>();
+        int answer = 0;
+
+        Map<Integer, Position> pointMap = new HashMap<>();
+        for (int i = 0; i < points.length; i++) {
+            pointMap.put(i + 1, new Position(points[i][0], points[i][1]));
         }
-        function(points, routes); // 경로 계산 
-        function2(); // 충돌 계산 
-        return answer;      
+
+        List<List<Position>> robotPaths = new ArrayList<>();
+
+        for (int[] route : routes) {
+            List<Position> path = new ArrayList<>();
+            Position current = pointMap.get(route[0]);
+            path.add(current);
+
+            for (int j = 1; j < route.length; j++) {
+                Position next = pointMap.get(route[j]);
+                path.addAll(calculatePath(current, next));
+                current = next;
+            }
+
+            robotPaths.add(path);
+        }
+
+        int maxTime = 0;
+        for (List<Position> path : robotPaths) {
+            maxTime = Math.max(maxTime, path.size());
+        }
+
+        for (int t = 0; t < maxTime; t++) {
+            Map<Position, Integer> positionCount = new HashMap<>();
+
+            for (List<Position> path : robotPaths) {
+                if (t < path.size()) {
+                    Position pos = path.get(t);
+                    positionCount.put(pos, positionCount.getOrDefault(pos, 0) + 1);
+                }
+            }
+
+            for (int count : positionCount.values()) {
+                if (count > 1) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
     }
-    public static void function2(){
-        int count = 0;
-        while(count != size){
-            int [][] map = new int [101][101];
-            count = 0;
-            for(int i = 0; i < size; i++){
-                if(memo[i].isEmpty()){
-                    count++;
-                    continue;
-                }
-                int [] temp = memo[i].poll();
-                map[temp[0]][temp[1]]++;
-            }
-            for(int i = 0; i < 101; i++){
-                for(int j = 0; j < 101; j++){
-                    if(map[i][j] > 1) answer++; // 충돌!
-                }
-            }
+
+    private List<Position> calculatePath(Position start, Position end) {
+        List<Position> path = new ArrayList<>();
+
+        int r = start.r;
+        int c = start.c;
+
+        while (r != end.r) {
+            if (r < end.r) r++;
+            else r--;
+            path.add(new Position(r, c));
         }
-    }
-    // 규칙4: 이동 우선순위 x좌표 > y좌표 
-    public static void function(int [][] points, int [][] routes){
-        for(int i = 0; i < size; i++){
-            int [] point = points[routes[i][0] - 1];
-            int x = point[0];
-            int y = point[1];
-            memo[i].add(new int[]{x, y});
-            for(int j = 1; j < routes[0].length; j++){
-                int [] next_point = points[routes[i][j] - 1];
-                int nx = next_point[0];
-                int ny = next_point[1];
-                
-                int xp = nx - x; // 다음 포인트로 이동해야하는 x좌표 
-                int yp = ny - y; // 다음 포인트로 이동해야하는 y좌표
-                while(xp != 0){
-                    if(xp > 0){
-                        xp--;
-                        x++;
-                        memo[i].add(new int[]{x, y});
-                    }
-                    else{
-                        xp++;
-                        x--;
-                        memo[i].add(new int[]{x, y});
-                    }
-                }
-                while(yp != 0){
-                    if(yp > 0){
-                        yp--;
-                        y++;
-                        memo[i].add(new int[]{x, y});
-                    }
-                    else{
-                        yp++;
-                        y--;
-                        memo[i].add(new int[]{x, y});
-                    }
-                }
-            }
+
+        while (c != end.c) {
+            if (c < end.c) c++;
+            else c--;
+            path.add(new Position(r, c));
         }
+
+        return path;
     }
 }
